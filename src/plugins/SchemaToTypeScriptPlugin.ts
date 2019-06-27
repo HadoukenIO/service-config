80ff27cf868427e57637cfbf4aeb602cf3612a77
ryanpetersonOF <ryan@openfin.co>
2019-06-27 12:24:32 +0100

    SchemaToTypescript Plugin

--- src/plugins/SchemaToTypeScriptPlugin.ts
+++ src/plugins/SchemaToTypeScriptPlugin.ts
@@ -1,34 +1,26 @@
 import * as path from 'path';
 import {compileFromFile} from 'json-schema-to-typescript';
 
-import {BasePlugin} from 'openfin-service-tooling/plugins/BasePlugin';
+import {BasePlugin, PluginOptions} from 'openfin-service-tooling/plugins/BasePlugin';
 
-
+type SchemaToTypeScriptOptions = {
+    schemaRoot: string;
+}
 /**
  * Webpack plugin to generate TypeScript code from one or more JSON schema files.
- * 
- * Supported options:
- * - outputPath: string
- *   Where to write generated .d.ts files to, either a filename or a directory.
- *   If a directory, filenames will be appended based on the input file with the extension changed.
- * - input: string|string[]
- *   The JSON Schema file(s) to generate TypeScript definitions for.
- *   If passing multiple files, then 'outputPath' MUST be a directory rather than an absolute filename.
- * - schemaRoot: string
- *   Root directory to use when resolving "$ref" tags
  */
-export class SchemaToTypeScriptPlugin extends BasePlugin {
-    constructor(options: any) {
+export class SchemaToTypeScriptPlugin extends BasePlugin<PluginOptions<SchemaToTypeScriptOptions>> {
+    constructor(options: PluginOptions<SchemaToTypeScriptOptions>) {
         super('SchemaToTypeScriptPlugin', ".d.ts", options);
 
         // Load plugin-specific options
-        this.parseOptions(options, {
+        this.parseOptions<SchemaToTypeScriptOptions>(options, {
             schemaRoot: 'string'
         });
     }
 
     async run() {
-        await Promise.all(this.options.input.map(async (schemaFilename: string) => {
+        await Promise.all((this.options.input as string[]).map(async (schemaFilename: string) => {
             console.log(`Generating TypeScript definitions for ${path.basename(schemaFilename)}`);
             
             const output = await compileFromFile(schemaFilename, {cwd: this.options.schemaRoot});
